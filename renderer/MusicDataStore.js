de8069a9e1cad7f7ef699c08b24f64daa2ca537f
aschenmaker <aschen@cug.edu.cn>
2020-03-23 21:54:14 +0800

    音乐持久化存储

---
+++ renderer/MusicDataStore.js
@@ -0,0 +1,34 @@
+const Store = require('electron-store');
+const uuidv4 = require('uuid/v4');
+const path = require('path');
+class DataStore extends Store {
+	constructor(settings) {
+		super(settings);
+		this.tracks = this.get('tracks') || [];
+	}
+	saveTracks() {
+		this.set('tracks', this.tracks);
+		return this;
+	}
+	getTracks() {
+		return this.get('tracks') || [];
+	}
+	addTracks(tracks) {
+		const trackWithProps = tracks
+			.map((track) => {
+				return {
+					id: uuidv4(),
+					path: track,
+					fileName: path.basename(track)
+				};
+			})
+			.filter((track) => {
+				const currentTracksPath = this.getTracks().map((track) => track.path);
+				return currentTracksPath.indexOf(track.path) < 0;
+			});
+		this.tracks = [ ...this.tracks, ...trackWithProps ];
+		return this.saveTracks();
+	}
+}
+
+module.exports = DataStore;
